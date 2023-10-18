3574108eba4bd201473be7555368ed478b4e9b40
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-18 15:37:19 -0400

    feat: apply binding of http services to all app

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,8 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:uno/uno.dart';
 
+import 'core/services/http/http.dart';
+import 'core/services/http/uno.dart';
 import 'modules/welcome/welcome_module.dart';
 
 final class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton(Uno.new);
+    i.addLazySingleton<IHttpService>(UnoService.new);
+
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.module(Modular.initialRoute, module: WelcomeModule());
