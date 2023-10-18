67f8355ba07b4744c56d6ccf4c5fef9279f5f34a
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-18 15:39:33 -0400

    feat: add binding to Search Packages Repository

--- lib/src/modules/welcome/welcome_module.dart
+++ lib/src/modules/welcome/welcome_module.dart
@@ -1,8 +1,20 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../app_module.dart';
+import '../../core/data/repositories/search_packages/search_packages_repository.dart';
 import 'presentation/pages/welcome_page.dart';
 
 final class WelcomeModule extends Module {
+  @override
+  List<Module> get imports => [AppModule()];
+
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton(SearchPackagesRepository.new);
+
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.child(
