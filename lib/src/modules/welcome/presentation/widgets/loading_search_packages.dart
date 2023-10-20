4e13214cea5f8ec32e8174237542b0f99188bbc3
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-20 11:07:12 -0400

    feat: add laoding search packages widget

---
+++ lib/src/modules/welcome/presentation/widgets/loading_search_packages.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../../core/themes/app_images.dart';
+import '../../../../core/widgets/lotties_widgets/lottie_assets.dart';
+
+class LoadingSearchPackages extends StatelessWidget {
+  const LoadingSearchPackages({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LottieAssetsWidget(
+            pathAnimation: AppImage.laoding,
+            width: MediaQuery.of(context).size.width * .7,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              AppLocalizations.of(context)!.searchingPackage,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
