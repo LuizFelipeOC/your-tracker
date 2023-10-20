7b1a6f7d60929d473f353f508b4375e898863702
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-20 15:27:18 -0400

    refactor: add sad-face image path

--- lib/src/core/themes/app_images.dart
+++ lib/src/core/themes/app_images.dart
@@ -1,5 +1,6 @@
 final class AppImage {
   static String get welcomeImage => 'assets/images/delivery 1.png';
+  static String get sadFace => 'assets/images/sad-face.png';
 
   static String get laoding => 'assets/lotties/loading.json';
 }
