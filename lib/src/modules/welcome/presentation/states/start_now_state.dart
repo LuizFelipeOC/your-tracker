d08eecd10c38e05117469bc0b4ad492e4e291b83
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-23 17:06:46 -0400

    test: add test start now controller

---
+++ lib/src/modules/welcome/presentation/states/start_now_state.dart
@@ -0,0 +1,9 @@
+abstract class StartNowState {}
+
+class IdleStartNowState implements StartNowState {}
+
+class LoadingNowState implements StartNowState {}
+
+class LoadedNowState implements StartNowState {}
+
+class ErrorNowState implements StartNowState {}
