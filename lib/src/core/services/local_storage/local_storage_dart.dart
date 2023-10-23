65d775e127c9221eca24f89e83b2e5a20e29e497
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-23 11:12:59 -0400

    test: add local secure storage test

---
+++ lib/src/core/services/local_storage/local_storage_dart.dart
@@ -0,0 +1,22 @@
+import 'package:result_dart/result_dart.dart';
+
+abstract interface class ILocalStorage {
+  AsyncResult<SuccessSaveData, FailureSaveData> save({required String key, required dynamic value});
+  AsyncResult<SuccessReadData, FailureReadData> read({required String key});
+}
+
+final class SuccessSaveData {}
+
+final class FailureSaveData {}
+
+final class SuccessReadData {
+  String data;
+
+  SuccessReadData({required this.data});
+}
+
+final class FailureReadData {
+  String message;
+
+  FailureReadData({required this.message});
+}
