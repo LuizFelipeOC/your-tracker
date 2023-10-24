849e674c2236223aa721a01e3a1ab86d66aed8e0
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-24 10:09:10 -0400

    feat: add DP of Controller and Repository

--- lib/src/modules/splash/splash_module.dart
+++ lib/src/modules/splash/splash_module.dart
@@ -1,16 +1,24 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../app_module.dart';
+import 'data/repositories/splash_repository.dart';
+import 'presentation/controller/splash_controller.dart';
 import 'presentation/pages/splash_page.dart';
 
 class SplashModules extends Module {
   @override
+  List<Module> get imports => [AppModule()];
+
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton(SplashRepository.new);
+    i.addLazySingleton(SplashController.new);
+    super.binds(i);
+  }
+
+  @override
   void routes(RouteManager r) {
-    r.child(
-      '/',
-      child: (_) => const SplashPage(),
-      transition: TransitionType.fadeIn,
-      duration: const Duration(milliseconds: 350),
-    );
+    r.child('/', child: (_) => const SplashPage());
     super.routes(r);
   }
 }
