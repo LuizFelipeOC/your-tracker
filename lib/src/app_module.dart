54c134fbe6d8c689f3189a7b7bef7a87b8e47b4e
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-10-24 10:08:36 -0400

    refactor: configure WelcomeModule and apply transitions in others modules

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -8,6 +8,7 @@ import 'core/services/local_storage/flutter_secure_storage.dart';
 import 'core/services/local_storage/local_storage_dart.dart';
 import 'modules/home/home_module.dart';
 import 'modules/splash/splash_module.dart';
+import 'modules/welcome/welcome_module.dart';
 
 final class AppModule extends Module {
   @override
@@ -22,7 +23,18 @@ final class AppModule extends Module {
 
   @override
   void routes(RouteManager r) {
-    r.module(Modular.initialRoute, module: SplashModules());
+    r.module(
+      Modular.initialRoute,
+      module: SplashModules(),
+      transition: TransitionType.fadeIn,
+      duration: const Duration(milliseconds: 200),
+    );
+    r.module(
+      '/welcome/',
+      module: WelcomeModule(),
+      transition: TransitionType.fadeIn,
+      duration: const Duration(milliseconds: 200),
+    );
     r.module(
       '/home/',
       module: HomeModule(),
