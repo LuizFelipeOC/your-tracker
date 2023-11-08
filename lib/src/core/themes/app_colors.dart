b6afa355d9910eb2427ee84a01c3b0718c708252
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-08 15:42:33 -0400

    feat: add orange color

--- lib/src/core/themes/app_colors.dart
+++ lib/src/core/themes/app_colors.dart
@@ -8,6 +8,7 @@ final class AppColors {
   static Color red = const Color(0xFFfa2542);
   static Color white = const Color(0xFFF2F2F2);
   static Color green = const Color(0xFF75eb86);
+  static Color orange = const Color(0xFFed330e);
 
   static LinearGradient textGradient = LinearGradient(
     begin: Alignment.topLeft,
