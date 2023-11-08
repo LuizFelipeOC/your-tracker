5f6626527cc96a1bb11272a8f447d58e2248a841
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-08 15:06:37 -0400

    refactor: transform items in components
    improve read the code in futures maintenance

--- lib/src/modules/home/presentation/pages/home_page.dart
+++ lib/src/modules/home/presentation/pages/home_page.dart
@@ -3,12 +3,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../core/data/models/packages_model.dart';
-import '../../../../core/themes/app_colors.dart';
 import '../../../../core/widgets/modal_search_packages/modal_search_packages.dart';
-import '../../../../core/widgets/modal_search_packages/states_widgets/loading_search_packages.dart';
 import '../controller/home_controller.dart';
-import '../controller/states/home_state.dart';
-import '../widgets/simple_card_tracking.dart';
+import '../widgets/home_bar.dart';
+import '../widgets/list_codes_tracking_saved.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -39,91 +37,9 @@ class _HomePageState extends State<HomePage> {
           height: screen.height,
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
-                alignment: Alignment.centerLeft,
-                child: Text.rich(
-                  TextSpan(
-                    text: AppLocalizations.of(context)!.firstWelcomeText,
-                    style: Theme.of(context).textTheme.headlineSmall,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: AppLocalizations.of(context)!.secondWelcomeText,
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.start,
-                ),
-              ),
+              const HomeBarWidget(),
               Expanded(
-                child: ValueListenableBuilder(
-                  valueListenable: homeStore,
-                  builder: (ctx, state, _) {
-                    if (state is LoadingHomeState) {
-                      return Center(
-                        child: LoadingSearchPackages(
-                          title: AppLocalizations.of(context)!.loadingMyPackages,
-                        ),
-                      );
-                    }
-
-                    if (state is ErrorHomeState) {
-                      return Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              state.message == 'Dont have items in cahced' ? AppLocalizations.of(context)!.emptyHomeItems : AppLocalizations.of(context)!.errorHomeItems,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-
-                    if (state is SuccessHomeState) {
-                      if (state.list.isEmpty) {
-                        return Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 20),
-                                child: Text(
-                                  AppLocalizations.of(context)!.emptyHomeItems,
-                                  style: Theme.of(context).textTheme.headlineMedium,
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
-
-                      return RefreshIndicator(
-                        backgroundColor: AppColors.white,
-                        color: AppColors.primaryColor,
-                        onRefresh: () => homeStore.getAllFavoritePackages(),
-                        child: ListView.builder(
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          itemCount: state.list.length,
-                          itemBuilder: (ctx, index) {
-                            final item = state.list[index];
-
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                              child: SimpleCardTracking(item: item),
-                            );
-                          },
-                        ),
-                      );
-                    }
-
-                    return Container();
-                  },
-                ),
+                child: ListCodesTrackingSavedWidget(homeStore: homeStore),
               ),
             ],
           ),
@@ -149,6 +65,7 @@ class _HomePageState extends State<HomePage> {
             );
 
             if (object == null) {
+              await homeStore.getAllFavoritePackages();
               return;
             }
 
