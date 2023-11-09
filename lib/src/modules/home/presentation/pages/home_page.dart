955aa6885ee57dbcd087996e4396db1807112e62
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-09 16:57:48 -0400

    refactor: call new search-packages module

--- lib/src/modules/home/presentation/pages/home_page.dart
+++ lib/src/modules/home/presentation/pages/home_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../../search_packages/data/models/packages_model.dart';
-import '../../../../core/widgets/modal_search_packages/modal_search_packages.dart';
 import '../controller/home_controller.dart';
 import '../widgets/home_bar.dart';
 import '../widgets/list_codes_tracking_saved.dart';
@@ -52,28 +50,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(10),
         child: FloatingActionButton.extended(
-          onPressed: () async {
-            final object = await showModalBottomSheet<PackagesModel?>(
-              context: context,
-              isDismissible: false,
-              isScrollControlled: true,
-              useSafeArea: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              builder: (_) => const ModalSearchPackges(
-                isStarnedNow: true,
-                isView: false,
-              ),
-            );
-
-            if (object == null) {
-              await homeController.getAllFavoritePackages();
-              return;
-            }
-
-            homeController.addPackageInList(packages: object);
-          },
+          onPressed: () => Modular.to.pushNamed('/search-packages/', arguments: false),
           label: Text(AppLocalizations.of(context)!.buttonHomeAddCode),
         ),
       ),
