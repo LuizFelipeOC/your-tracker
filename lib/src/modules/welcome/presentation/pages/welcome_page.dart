2f103487756119c459eff1f5e77add1374c11c31
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-09 14:46:16 -0400

    refactor: organized structure per widgets

--- lib/src/modules/welcome/presentation/pages/welcome_page.dart
+++ lib/src/modules/welcome/presentation/pages/welcome_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../../../core/themes/app_images.dart';
-import '../../../../core/widgets/modal_search_packages/modal_search_packages.dart';
 import '../controller/welcome_controller.dart';
 import '../states/welcome_state.dart';
+import '../widgets/welcome_bottom.dart';
+import '../widgets/welcome_centered.dart';
+import '../widgets/welcome_header.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -43,98 +43,16 @@ class _WelcomePageState extends State<WelcomePage> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Expanded(
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Text.rich(
-                    TextSpan(
-                      text: AppLocalizations.of(context)!.firstWelcomeText,
-                      style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                          ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: AppLocalizations.of(context)!.secondWelcomeText,
-                          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                                fontSize: 36,
-                                fontWeight: FontWeight.bold,
-                              ),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                ),
+              const Expanded(
+                child: WelcomeHeaderWidget(),
               ),
               Expanded(
-                child: Column(
-                  children: [
-                    Center(
-                      child: Image.asset(
-                        AppImage.welcomeImage,
-                        width: screen.width * .45,
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    Text(
-                      AppLocalizations.of(context)!.subTitleWelcomeText.toUpperCase(),
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            fontSize: 18,
-                            letterSpacing: 0.98,
-                          ),
-                    )
-                  ],
-                ),
+                child: WelcomeCenteredMessageWidget(screen: screen),
               ),
               Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 56,
-                      width: screen.width * .80,
-                      child: ElevatedButton(
-                        onPressed: () => _welcomeController.startNow(),
-                        child: Text(
-                          AppLocalizations.of(context)!.welcomeFirstButton.toUpperCase(),
-                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                                letterSpacing: 2.0,
-                              ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      AppLocalizations.of(context)!.or.toUpperCase(),
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          useSafeArea: true,
-                          isScrollControlled: true,
-                          isDismissible: false,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-                          ),
-                          context: context,
-                          builder: (ctx) => const ModalSearchPackges(
-                            isStarnedNow: false,
-                            isView: false,
-                          ),
-                        );
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.simpleSearch.toUpperCase(),
-                        style: const TextStyle(
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ),
-                  ],
+                child: WelcomeBottomWidget(
+                  screen: screen,
+                  welcomeController: _welcomeController,
                 ),
               )
             ],
