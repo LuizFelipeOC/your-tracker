5c4523930ece4a502d8c51892404d165cbce9dda
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-10 11:50:28 -0400

    refactor: add animation in box messages

--- lib/src/core/widgets/messages_widgets/box_message_widgets.dart
+++ lib/src/core/widgets/messages_widgets/box_message_widgets.dart
@@ -1,47 +1,83 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 import '../../themes/app_images.dart';
 
-class BoxMessagesWidget extends StatelessWidget {
+class BoxMessagesWidget extends StatefulWidget {
   final IconData icon;
   final Color iconColor;
   final String title;
 
   const BoxMessagesWidget({
-    super.key,
+    Key? key,
     required this.icon,
-    required this.title,
     required this.iconColor,
-  });
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<BoxMessagesWidget> createState() => _BoxMessagesWidgetState();
+}
+
+class _BoxMessagesWidgetState extends State<BoxMessagesWidget> {
+  bool animationBox = false;
+  bool animationText = false;
+
+  void _animtation() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    setState(() {
+      animationText = true;
+      animationBox = true;
+    });
+  }
+
+  @override
+  void initState() {
+    _animtation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Stack(
-          children: [
-            Image.asset(
-              AppImage.box,
-              width: MediaQuery.of(context).size.width * 0.3,
-            ),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Icon(
-                icon,
-                color: iconColor,
-                size: 50,
+        AnimatedCrossFade(
+          firstChild: Stack(
+            children: [
+              Image.asset(
+                AppImage.box,
+                width: MediaQuery.of(context).size.width * 0.26,
               ),
-            ),
-          ],
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Icon(
+                  widget.icon,
+                  color: widget.iconColor,
+                  size: 46,
+                ),
+              ),
+            ],
+          ),
+          secondChild: Container(width: MediaQuery.of(context).size.width * 0.26),
+          sizeCurve: Curves.fastLinearToSlowEaseIn,
+          crossFadeState: animationBox ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+          duration: const Duration(seconds: 1),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.labelLarge,
-            textAlign: TextAlign.center,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+          child: AnimatedCrossFade(
+            firstChild: Text(
+              widget.title,
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(letterSpacing: 2.25, fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            secondChild: Container(height: 60),
+            sizeCurve: Curves.fastLinearToSlowEaseIn,
+            crossFadeState: animationText ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            duration: const Duration(seconds: 1),
           ),
         ),
       ],
