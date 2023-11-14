cc0051ba7bd6b258cf11855cf1cbc69606b5276c
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-14 16:28:36 -0400

    refactor: add initials bindings when start app module

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,3 +1,24 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:uno/uno.dart';
 
-final class AppModule extends Module {}
+import 'core/services/cache/cache_interface.dart';
+import 'core/services/cache/cache_service.dart';
+import 'core/services/uno/http_interface.dart';
+import 'core/services/uno/uno_service.dart';
+import 'modules/welcome/welcome_module.dart';
+
+final class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addSingleton(Uno.new);
+    i.addSingleton<IHttpService>(UnoService.new);
+    i.addSingleton<ICache>(CacheService.new);
+    super.binds(i);
+  }
+
+  @override
+  void routes(RouteManager r) {
+    r.module(Modular.initialRoute, module: WelcomeModule());
+    super.routes(r);
+  }
+}
