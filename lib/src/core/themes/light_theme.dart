0ce1ea64059275d8a1e9152c874e9eac92bc3cdd
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-16 16:21:07 -0400

    feat: :lipstick: added input theme and label large theme

--- lib/src/core/themes/light_theme.dart
+++ lib/src/core/themes/light_theme.dart
@@ -9,6 +9,27 @@ final class LightTheme {
         textTheme: _textTheme(),
         elevatedButtonTheme: _elevatedButtonTheme(),
         textButtonTheme: _textButtonnTheme(),
+        inputDecorationTheme: _inputDecorationTheme(),
+      );
+
+  static InputDecorationTheme _inputDecorationTheme() => InputDecorationTheme(
+        filled: true,
+        fillColor: AppColors.secondary.withOpacity(0.2),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: AppColors.primary.withOpacity(0.2),
+            width: 0.4,
+          ),
+        ),
+        hintStyle: TextStyle(
+          color: AppColors.secondary,
+          fontWeight: FontWeight.w300,
+        ),
       );
 
   static TextButtonThemeData _textButtonnTheme() => TextButtonThemeData(
@@ -54,6 +75,16 @@ final class LightTheme {
           fontSize: 22,
           color: AppColors.secondary,
           fontWeight: FontWeight.w500,
+        ),
+        labelLarge: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w800,
+          color: AppColors.primary,
+        ),
+        labelSmall: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: AppColors.primary,
         ),
       );
 }
