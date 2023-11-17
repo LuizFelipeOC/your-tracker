39209be953e653f7aad1afc5de0f6bca4336ac87
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-17 08:55:41 -0400

    feat: add AnimationEntraceController binding

--- lib/src/modules/welcome/welcome_module.dart
+++ lib/src/modules/welcome/welcome_module.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'presentation/controller/animation_entrace_controller.dart';
 import 'presentation/pages/welcome_page.dart';
 
 class WelcomeModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.add(AnimationEntranceController.new);
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.child('/', child: (_) => const WelcomePage());
