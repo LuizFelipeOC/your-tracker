1174520b7ea1a11ead0a36953b474148deb2d649
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-11-17 12:06:02 -0400

    refactor: apply intl texts

--- lib/src/modules/home/presentation/components/drawers/unauthenticated_body_drawer.dart
+++ lib/src/modules/home/presentation/components/drawers/unauthenticated_body_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../../core/constants/app_colors.dart';
 import '../../../../../core/widgets/font_awesome/font_awesome.dart';
@@ -35,29 +36,43 @@ class UnauthenticatedBodyDrawer extends StatelessWidget {
           const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.all(20),
-            child: Text(
-              'Acho que voce nao esta logado no momento, crie usa conta agora mesmo ou acesse uma ja existente',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                    fontSize: 14,
-                    color: AppColors.primary,
-                    fontWeight: FontWeight.bold,
+            child: Text.rich(
+              TextSpan(
+                text: AppLocalizations.of(context)!.unauthenticatedFirstMessage,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontSize: 16,
+                      color: AppColors.primary.withOpacity(0.9),
+                      fontWeight: FontWeight.w700,
+                    ),
+                children: [
+                  TextSpan(
+                    text: AppLocalizations.of(context)!.unauthenticatedSecondMessage,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontSize: 16,
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
                   ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
           ElevatedButton(
             onPressed: () {},
-            child: const Text('Registrar'),
+            child: Text(AppLocalizations.of(context)!.registerMessage),
           ),
-          const Text('ou'),
+          const SizedBox(height: 5),
+          Text(AppLocalizations.of(context)!.or),
+          const SizedBox(height: 5),
           TextButton(
             onPressed: () {},
             child: Text(
-              'Entrar em uma conta ja existente',
+              AppLocalizations.of(context)!.alreadyAccountMessage,
               style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                    fontSize: 14,
+                    fontSize: 16,
                     color: AppColors.primary,
-                    fontWeight: FontWeight.w400,
+                    fontWeight: FontWeight.w600,
                   ),
             ),
           ),
