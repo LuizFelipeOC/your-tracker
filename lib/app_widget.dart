408d4c3ccaf2875bf5bc5fdefc623f463e840c10
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-06-25 15:35:01 -0400

    refactor: apply light app theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'app/pages/onboarding/onboarding.dart';
+import 'app/utils/light_theme.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -8,6 +9,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: LightTheme.themeData,
       debugShowCheckedModeBanner: false,
       home: OnboardingPage(),
     );
