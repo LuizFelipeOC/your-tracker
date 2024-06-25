304eb30cdfbbe13336aa296e701144c99d700434
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-06-25 15:05:20 -0400

    feat: init project

---
+++ android/app/src/main/kotlin/com/cronw/your_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cronw.your_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'app/pages/onboarding/onboarding.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    );
+  }
+}
