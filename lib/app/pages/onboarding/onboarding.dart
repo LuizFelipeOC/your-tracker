db2d651e2843cc1089193ca62f7d5175cfd5a2ee
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-06-25 15:41:52 -0400

    refactor: add fade entrance animation

--- lib/app/pages/onboarding/onboarding.dart
+++ lib/app/pages/onboarding/onboarding.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../utils/app_images.dart';
+import '../../utils/media_query_values.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -10,6 +14,56 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Column(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    AppImages.onboardingImage,
+                    width: context.width * .4,
+                  ).animate().fade(
+                        delay: Duration(milliseconds: 350),
+                        duration: Duration(seconds: 1),
+                      ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Bem-Vindo ao Your Tracker',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ).animate().fade(
+                      delay: Duration(milliseconds: 350),
+                      duration: Duration(seconds: 2),
+                    ),
+                Text(
+                  'Controle suas encomendas do melhor jeito',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ).animate().fade(
+                      delay: Duration(milliseconds: 350),
+                      duration: Duration(seconds: 3),
+                    ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          SizedBox(
+            height: 52,
+            width: context.width * .90,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('COMEÇAR AGORA'),
+            ),
+          ).animate().fade(
+                delay: Duration(milliseconds: 450),
+                duration: Duration(seconds: 3),
+              ),
+        ],
+      ),
+    );
   }
 }
